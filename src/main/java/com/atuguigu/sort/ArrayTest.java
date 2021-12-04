51daec560f3e51009eb2adbe708cf907ae8f80e4
xiao-pang007 <wangguoping@hrfax.cn>
2021-12-04 14:21:31 +0800

    DataSource's first commit

---
+++ src/main/java/com/atuguigu/sort/ArrayTest.java
@@ -0,0 +1,15 @@
+package com.atuguigu.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author xiaopang
+ * @create 2021-12-03-20:21
+ **/
+public class ArrayTest {
+    public static void main(String[] args) {
+        final int[] dsa  = new int[1];
+//        args[0]  = 21;
+//        System.out.println(Arrays.toString(args[0]));
+    }
+}
---
+++ src/main/java/com/atuguigu/sort/GitTest.java
@@ -0,0 +1,11 @@
+package com.atuguigu.sort;
+
+/**
+ * @author xiaopang
+ * @create 2021-12-04-14:18
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("ds");
+    }
+}
